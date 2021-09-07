524efe13614096d2eba722f59ba45114ecc9cdfc
vinayaka-sm <57322769+vinayaka-sm@users.noreply.github.com>
2021-09-07 19:38:45 +0530

    Update ll.h

--- 3_Implementation/inc/ll.h
+++ 3_Implementation/inc/ll.h
@@ -1,36 +1,7 @@
 #include "student.h"
-/**
- * @brief stores the student data at the beginning of Linked List
- * 
- * @param s (Student structure)
- */
 void insert_at_begin(Student s);
-/**
- * @brief stores the student data at the end of Linked list
- * 
- * @param s 
- */
 void insert_at_end(Student s);
-/**
- * @brief to traverse across each student data
- * 
- */
 void traverse();
-/**
- * @brief to delete the student data based on their roll number
- * 
- * @param rollNo 
- */
 void delete(int rollNo);
-/**
- * @brief to delete particular student data
- * 
- * @param head_ref 
- * @param position 
- */
 void deleteNode(struct Node **head_ref, int position);
-/**
- * @brief to print all required information
- * 
- */
 void print();
